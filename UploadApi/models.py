fc5ecdce50b7715142245e8097436c0f2c8e068c
Marcin Potoczny <marcin.potoczny@protonmail.com>
2023-02-14 12:23:00 +0100

    first

---
+++ UploadApi/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class UploadedImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.image
+        
+
